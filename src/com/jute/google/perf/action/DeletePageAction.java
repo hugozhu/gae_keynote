f7c5558703a7c62946a65b6fb66ed0da3de6a3a0
Hugo Zhu <hugozhu@gmail.com>
2009-07-02 00:32:23 +0800

    clean up

--- src/com/jute/google/perf/action/DeletePageAction.java
+++ src/com/jute/google/perf/action/DeletePageAction.java
@@ -3,9 +3,6 @@ package com.jute.google.perf.action;
 import com.jute.google.framework.Action;
 import com.jute.google.framework.PMF;
 import com.jute.google.perf.model.Page;
-import com.jute.google.perf.model.DataPoint;
-import com.jute.google.util.HTTPResponse;
-import com.jute.google.util.URLFetcher;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,8 +10,6 @@ import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
 import java.util.Map;
 import java.util.List;
-import java.util.Properties;
-import java.net.URL;
 
 /**
  * User: hugozhu
